27487b949a3ef36b5249d3aef4bd74fba1ebd232
Spatocode <impulseofficial069@gmail.com>
2018-08-31 00:01:30 +0100

    Added app logic

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,31 @@
-const zipCode = document.querySelector('.zipCode')
-const weather = document.querySelector('.zipCode')
-const condition = document.querySelector('.condition')
-const degree = document.querySelector('.degree')
+const input = document.querySelector('.zip')
+const main = document.querySelector('.main')
+const desc = document.querySelector('.description')
+const temp = document.querySelector('.temperature')
+const hum = document.querySelector('.humidity')
 const WEATHER_API_KEY = "bbeb34ebf60ad50f7893e7440a1e2b0b";
 const API_STEM = "http://api.openweathermap.org/data/2.5/weather?";
+
+input.addEventListener('change',()=>{
+    let zip = input.value
+    fetchForecast(zip)
+})
+
+function zipUrl(code) {
+    return `${API_STEM}q=${code}&units=imperial&APPID=${WEATHER_API_KEY}`;
+}
+
+function fetchForecast(code) {
+    return fetch(zipUrl(code))
+      .then(res => res.json())
+      .then(data => {
+        main.innerHTML = data.weather[0].main
+        desc.innerHTML = data.weather[0].description
+        temp.innerHTML = `${data.main.temp}°F`
+        hum.innerHTML = `${data.main.humidity}%`
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  23401
