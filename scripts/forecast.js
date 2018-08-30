b1d30a1479dabbce9aa86034153ef6c6c22a73c3
Spatocode <impulseofficial069@gmail.com>
2018-08-30 20:14:08 +0100

    Included DOM elements

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,2 +1,6 @@
+const zipCode = document.querySelector('.zipCode')
+const weather = document.querySelector('.zipCode')
+const condition = document.querySelector('.condition')
+const degree = document.querySelector('.degree')
 const WEATHER_API_KEY = "bbeb34ebf60ad50f7893e7440a1e2b0b";
 const API_STEM = "http://api.openweathermap.org/data/2.5/weather?";
